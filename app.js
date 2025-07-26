929d6411e61be24547f4bdb3fded1d838909350d
Manpreet <itsmanpreet643@gmail.com>
2025-07-27 02:20:10 +0530

    fix: use env variable for MongoDB connection

--- app.js
+++ app.js
@@ -56,22 +56,22 @@ const listingRouter = require("./routes/listings.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-main().then(() => {
-    console.log("connected to Database");
-})
-.catch((err) => {
-    console.log(err);
-});
-
-
-async function main() {
-    await mongoose.connect(MONGO_URL);
-}
 
 // Middleware setup already done above
 
+const dbUrl = process.env.MONGO_URL;
+
+main()
+  .then(() => {
+    console.log(" Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+async function main() {
+  await mongoose.connect(dbUrl);
+};
 
 
 
